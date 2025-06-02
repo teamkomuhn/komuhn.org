e7171b98fad2d578ee947f4339003ac204fe93fb
Ayo Reis <hey@ayoreis.com>
2025-06-02 13:14:34 +0100

    Cleanup Fresh template

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,17 +1,19 @@
-import type { PageProps } from "fresh";
+import { define } from "../utils.ts";
 
-export default function App({ Component }: PageProps) {
-  return (
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>komuhn.org</title>
-        <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>
-        <Component />
-      </body>
-    </html>
-  );
-}
+export default define.page(({ Component }) => {
+	return (
+		<html lang="en">
+			<head>
+				<meta name="viewport" content="width=device-width" />
+				<title>Komuhn</title>
+				<link rel="stylesheet" href="/styles.css" />
+			</head>
+
+			<body>
+				<main>
+					<Component />
+				</main>
+			</body>
+		</html>
+	);
+});
--- utils.ts
+++ utils.ts
@@ -2,5 +2,4 @@ import { createDefine } from "fresh";
 
 // deno-lint-ignore no-empty-interface
 export interface State {}
-
 export const define = createDefine<State>();
