53d45e856d9f494def7bc611e6608f3eda38caa9
Ayo Reis <hey@ayoreis.com>
2025-06-11 18:41:35 +0100

    Switch from custom markdown pipeline to MDX and esbuild

--- main.ts
+++ main.ts
@@ -1,5 +1,55 @@
+import esbuild from "esbuild";
+import mdx from "@mdx-js/esbuild";
+import remark_frontmatter from "remark-frontmatter";
+import remark_mdx_frontmatter from "remark-mdx-frontmatter";
+import remark_smartypants from "remark-smartypants";
+import unist_util_flatmap from "unist-util-flatmap";
 import { App, fsRoutes, staticFiles } from "@fresh/core";
 import type { State } from "./utils.ts";
+
+const promises: Promise<unknown>[] = [];
+
+function remark_flatten_image_paragraphs() {
+	// @ts-expect-error: TODO
+	return (tree) => {
+		// @ts-expect-error: TODO
+		return unist_util_flatmap(tree, (node) => {
+			if (
+				node.type === "paragraph" &&
+				// @ts-expect-error: TODO
+				node.children.every((child) =>
+					child.type === "image" ||
+					child.type === "mdxJsxFlowElement" ||
+					child.type === "mdxJsxTextElement" ||
+					child.type === "text" && child.value === "\n"
+				)
+			) {
+				return node.children;
+			}
+
+			return [node];
+		});
+	};
+}
+
+const context = await esbuild.context({
+	entryPoints: ["content/projects/**"],
+	plugins: [
+		mdx({
+			jsxImportSource: "preact",
+			remarkPlugins: [
+				remark_frontmatter,
+				remark_mdx_frontmatter,
+				[remark_smartypants, { backticks: false, dashes: "oldschool" }],
+				remark_flatten_image_paragraphs,
+			],
+		}),
+	],
+	outdir: "_content",
+	outbase: "content",
+});
+
+promises.push(context.watch());
 
 export const app = new App<State>();
 
@@ -11,5 +61,7 @@ await fsRoutes(app, {
 });
 
 if (import.meta.main) {
-	await app.listen();
+	promises.push(app.listen());
 }
+
+await Promise.all(promises);
--- routes/projects/index.tsx
+++ routes/projects/index.tsx
@@ -1,7 +1,5 @@
 import type { JSX } from "preact/jsx-runtime";
 import { page } from "@fresh/core";
-import { parse } from "@std/path";
-import { extractYaml } from "@std/front-matter";
 import { define } from "../../utils.ts";
 
 export interface Frontmatter {
@@ -15,7 +13,7 @@ export interface Frontmatter {
 	partners?: string[];
 }
 
-export const PROJECTS_DIRECTORY = "content/projects";
+export const PROJECTS_DIRECTORY = "_content/projects";
 
 export const handler = define.handlers({
 	GET(context) {
@@ -28,10 +26,9 @@ export default define.page(async () => {
 	const projects: JSX.Element[] = [];
 
 	for await (const entry of Deno.readDir(PROJECTS_DIRECTORY)) {
-		const slug = parse(entry.name).name;
-		const path = `${PROJECTS_DIRECTORY}/${entry.name}`;
-		const content = await Deno.readTextFile(path);
-		const frontmatter = extractYaml<Frontmatter>(content).attrs;
+		const slug = entry.name.slice(0, -3);
+		const path = `../../${PROJECTS_DIRECTORY}/${entry.name}`;
+		const { frontmatter } = await import(path) as { frontmatter: Frontmatter };
 
 		projects.push(
 			<li key={slug}>
