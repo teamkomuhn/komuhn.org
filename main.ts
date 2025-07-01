428ba4aad553b3f244bbacbfe6f106cd1017148c
Ayo Reis <ayovreis@gmail.com>
2025-07-01 13:27:47 +0100

    Let's try once more

--- main.ts
+++ main.ts
@@ -1,53 +1,53 @@
-import esbuild from "esbuild";
-import mdx from "@mdx-js/esbuild";
-import remark_frontmatter from "remark-frontmatter";
-import remark_mdx_frontmatter from "remark-mdx-frontmatter";
-import remark_smartypants from "remark-smartypants";
-import unist_util_flatmap from "unist-util-flatmap";
+// import esbuild from "esbuild";
+// import mdx from "@mdx-js/esbuild";
+// import remark_frontmatter from "remark-frontmatter";
+// import remark_mdx_frontmatter from "remark-mdx-frontmatter";
+// import remark_smartypants from "remark-smartypants";
+// import unist_util_flatmap from "unist-util-flatmap";
 import { App, fsRoutes, staticFiles } from "@fresh/core";
 import type { State } from "./utils.ts";
 
-const promises: Promise<unknown>[] = [];
+// const promises: Promise<unknown>[] = [];
 
-function remark_flatten_image_paragraphs() {
-	// @ts-expect-error: TODO
-	return (tree) => {
-		// @ts-expect-error: TODO
-		return unist_util_flatmap(tree, (node) => {
-			if (
-				node.type === "paragraph" &&
-				// @ts-expect-error: TODO
-				node.children.every((child) =>
-					child.type === "image" ||
-					child.type === "mdxJsxFlowElement" ||
-					child.type === "mdxJsxTextElement" ||
-					child.type === "text" && child.value === "\n"
-				)
-			) {
-				return node.children;
-			}
+// function remark_flatten_image_paragraphs() {
+// 	// @ts-expect-error: TODO
+// 	return (tree) => {
+// 		// @ts-expect-error: TODO
+// 		return unist_util_flatmap(tree, (node) => {
+// 			if (
+// 				node.type === "paragraph" &&
+// 				// @ts-expect-error: TODO
+// 				node.children.every((child) =>
+// 					child.type === "image" ||
+// 					child.type === "mdxJsxFlowElement" ||
+// 					child.type === "mdxJsxTextElement" ||
+// 					child.type === "text" && child.value === "\n"
+// 				)
+// 			) {
+// 				return node.children;
+// 			}
 
-			return [node];
-		});
-	};
-}
+// 			return [node];
+// 		});
+// 	};
+// }
 
-const context = await esbuild.context({
-	entryPoints: ["content/projects/**"],
-	plugins: [
-		mdx({
-			jsxImportSource: "preact",
-			remarkPlugins: [
-				remark_frontmatter,
-				remark_mdx_frontmatter,
-				[remark_smartypants, { backticks: false, dashes: "oldschool" }],
-				remark_flatten_image_paragraphs,
-			],
-		}),
-	],
-	outdir: "_content",
-	outbase: "content",
-});
+// const context = await esbuild.context({
+// 	entryPoints: ["content/projects/**"],
+// 	plugins: [
+// 		mdx({
+// 			jsxImportSource: "preact",
+// 			remarkPlugins: [
+// 				remark_frontmatter,
+// 				remark_mdx_frontmatter,
+// 				[remark_smartypants, { backticks: false, dashes: "oldschool" }],
+// 				remark_flatten_image_paragraphs,
+// 			],
+// 		}),
+// 	],
+// 	outdir: "_content",
+// 	outbase: "content",
+// });
 
 // promises.push(context.watch());
 
